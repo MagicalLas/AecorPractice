c8d077c45bf07ba56c91fe648da2970534442da1
Wonho ha <haho013039@gmail.com>
2019-07-11 11:28:49 +0900

    조언에 따라 imort 추가

--- src/main/scala/EventsourcedBooking.scala
+++ src/main/scala/EventsourcedBooking.scala
@@ -1,8 +1,11 @@
+import aecor.MonadActionReject
+import cats.implicits._
+
 class EventsourcedBooking[F[_]](implicit F: MonadActionReject[F, Option[BookingState], BookingEvent, BookingCommandRejection]) extends Booking[F] {
 
   import F._
 
-  def place(client: ClientId): F[Unit] = read.flatMap{
+  def place(client: ClientId): F[Unit] = read.flatMap {
     case _ => reject(BookingErrorDefault)
   }
 }
