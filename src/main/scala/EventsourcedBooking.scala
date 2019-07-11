ad61e886d0d0e064384104a28e00e833a149493f
Wonho ha <haho013039@gmail.com>
2019-07-11 10:46:16 +0900

    Create EventsourcedBooking.scala

---
+++ src/main/scala/EventsourcedBooking.scala
@@ -0,0 +1,8 @@
+
+
+class EventsourcedBooking[F[_]](implicit F: MonadActionReject[F, Option[BookingState], BookingEvent, BookingCommandRejection]) extends Booking[F] {
+
+  import F._
+
+  def place(client: ClientId): F[Unit] = ???
+}
