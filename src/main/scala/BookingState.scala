1e0b079c6c07f9ce3ce30edf6792ca4428b42ee9
Wonho ha <haho013039@gmail.com>
2019-07-11 11:58:23 +0900

    State추가

--- src/main/scala/BookingState.scala
+++ src/main/scala/BookingState.scala
@@ -1,3 +1,13 @@
 case class BookingState(clietId: ClientId)
 
 case class ClientId(value: String) extends AnyVal
+
+case class Ticket(seat: Seat, price: Money)
+
+case class Money(amount: BigDecimal) extends AnyVal
+
+case class Seat(row: Row, number: SeatNumber)
+
+case class Row(num: Int) extends AnyVal
+
+case class SeatNumber(num: Int) extends AnyVal
