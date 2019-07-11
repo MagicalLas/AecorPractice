1ff2f12fc0be37ff758695ab4efe1a73aa61da06
Wonho ha <haho013039@gmail.com>
2019-07-11 12:12:39 +0900

    BookingStatus구현

--- src/main/scala/BookingState.scala
+++ src/main/scala/BookingState.scala
@@ -1,4 +1,7 @@
 case class BookingState(clietId: ClientId)
+import cats.data.NonEmptyList
+import enumeratum._
+import scala.collection.immutable
 
 case class ClientId(value: String) extends AnyVal
 
@@ -11,3 +14,20 @@ case class Seat(row: Row, number: SeatNumber)
 case class Row(num: Int) extends AnyVal
 
 case class SeatNumber(num: Int) extends AnyVal
+
+sealed trait BookingStatus extends EnumEntry
+
+object BookingStatus extends Enum[BookingState] with CirceEnum[BookingState] {
+
+  case object AwaitingConfirmation extends BookingStatus
+
+  case object Confirmed extends BookingStatus
+
+  case object Denied extends BookingStatus
+
+  case object Canceled extends BookingStatus
+
+  case object Settled extends BookingStatus
+
+  def values: immutable.IndexedSeq[BookingState] = findValues
+}
