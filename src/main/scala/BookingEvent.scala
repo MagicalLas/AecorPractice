77feb59acd49a1a28829b8783cd84688f865dd92
Wonho ha <haho013039@gmail.com>
2019-07-15 15:58:10 +0900

    Settled이벤트 추가

--- src/main/scala/BookingEvent.scala
+++ src/main/scala/BookingEvent.scala
@@ -5,3 +5,5 @@ sealed trait BookingEvent extends Product with Serializable
 case class BookingPlaced(clientId: ClientId) extends BookingEvent
 
 case class BookingConfirmed(tickets: NonEmptyList[Ticket]) extends BookingEvent
+
+case object BookingSettled extends BookingEvent
