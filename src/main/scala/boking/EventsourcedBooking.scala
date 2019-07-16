e7fa6b0cc098673f454bc555105e97f736c399ca
Wonho ha <haho013039@gmail.com>
2019-07-16 14:43:59 +0900

    Implement EventsourcedBooking Class

--- src/main/scala/boking/EventsourcedBooking.scala
+++ src/main/scala/boking/EventsourcedBooking.scala
@@ -1,1 +1,12 @@
 package boking
+
+import aecor.MonadActionReject
+import cats.data.NonEmptyList
+
+class EventsourcedBooking[F[_]]
+(implicit F: MonadActionReject[F, Option[BookingState], BookingEvent, BookingCommandRejection])
+  extends Booking[F] {
+  def place(client: ClientId, seats: NonEmptyList[Seat]): F[Unit] = ???
+
+  def confirm(ticket: NonEmptyList[Ticket]): F[Unit] = ???
+}
