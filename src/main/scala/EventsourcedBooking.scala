8f04da4305d416abe438d96f13268a3ef0cd8ab8
Wonho ha <haho013039@gmail.com>
2019-07-16 09:09:12 +0900

    Event Handler place 매개변수 추가

--- src/main/scala/EventsourcedBooking.scala
+++ src/main/scala/EventsourcedBooking.scala
@@ -6,22 +6,24 @@ import cats.data.NonEmptyList
 import cats.syntax.all._
 
 
-
 class EventsourcedBooking[F[_]](implicit F: MonadActionReject[F, Option[BookingState], BookingEvent, BookingCommandRejection]) extends Booking[F] {
 
   import F._
+
   val ignore: F[Unit] = F.unit
 
   implicit val tempMoneyMonoid: Monoid[Money] = new Monoid[Money] {
     override def empty: Money = ???
+
     override def combine(x: Money, y: Money): Money = ???
   }
 
-  def place(client: ClientId): F[Unit] = read.flatMap {
-    case Some(_) => reject(BookingAlreadyExists)
-    case None => append(BookingPlaced(client))
-    case _ => reject(BookingErrorDefault)
-  }
+  def place(client: ClientId, seats: NonEmptyList[Seat]): F[Unit] =
+    read.flatMap {
+      case Some(_) => reject(BookingAlreadyExists)
+      case None => append(BookingPlaced(client, seats))
+      case _ => reject(BookingErrorDefault)
+    }
 
   def confirm(tickets: NonEmptyList[Ticket]): F[Unit] = state.flatMap {
     case AwaitingConfirmation => append(BookingConfirmed(tickets)) >>
